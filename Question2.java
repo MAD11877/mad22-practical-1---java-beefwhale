96584c26060bfcf6d5b32c8753f7220103fdd6e9
beefwhale <84565580+beefwhale@users.noreply.github.com>
2022-04-19 22:54:10 +0800

    Question2 test 1

--- Question2.java
+++ Question2.java
@@ -4,13 +4,14 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompts user for height and weight, calculate and print out the BMI of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
-    Scanner in = new Scanner(System.in);
-    double height = in.nextDouble();
+    
+    Scanner input1 = new Scanner(System.in);
+    Scanner input2 = new Scanner(System.in);
+    
+    double height = input1.nextDouble();
+    double weight = input2.nextDouble();
+    
+    double BMI = weight / height * height;
+    System.out.println(BMI);
   }
 }
