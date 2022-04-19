273cca39543b1798be51863dc3ddad24112b39ca
beefwhale <84565580+beefwhale@users.noreply.github.com>
2022-04-19 23:29:15 +0800

    Question 2 test 2

--- Question2.java
+++ Question2.java
@@ -8,10 +8,12 @@ public class Question2
     Scanner input1 = new Scanner(System.in);
     Scanner input2 = new Scanner(System.in);
     
+    System.out.println("Enter height:"); 
     double height = input1.nextDouble();
+    System.out.println("Enter weight:"); 
     double weight = input2.nextDouble();
     
-    double BMI = weight / height * height;
+    double BMI = weight / (height * height);
     System.out.println(BMI);
   }
 }
