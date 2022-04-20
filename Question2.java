c6faaf036c429b9182e11edfe7f8d43572216ee3
beefwhale <84565580+beefwhale@users.noreply.github.com>
2022-04-20 10:22:31 +0800

    Question 2 test idk

--- Question2.java
+++ Question2.java
@@ -8,9 +8,9 @@ public class Question2
     Scanner input1 = new Scanner(System.in);
     Scanner input2 = new Scanner(System.in);
     
-    System.out.println("Enter height:"); 
+    
     double height = input1.nextDouble();
-    System.out.println("Enter weight:"); 
+    
     double weight = input2.nextDouble();
     
     double BMI = weight / (height * height);
