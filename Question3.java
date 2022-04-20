20d43868b0694a95f6d7136750fee96bc57018e1
beefwhale <84565580+beefwhale@users.noreply.github.com>
2022-04-20 10:19:21 +0800

    Question 3 test 2

--- Question3.java
+++ Question3.java
@@ -4,6 +4,7 @@ public class Question3
 {
   public static void main(String[] args)
   {
+    Scanner in = new Scanner(System.in);
     int number = in.nextInt();
     int newnumber = number * number;
 
